bb54a601116883d990d3ae0d4d3667f5aee56bfc
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2023-01-11 15:04:14 -0400

    Novo componente de dialog

---
+++ src/components/DialogComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-dialog v-model="dialog" width="500" height="300">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn v-on="on" v-bind="attrs">Open</v-btn>
+    </template>
+    <v-card>
+      <v-toolbar dense color="elevation-0">
+        <v-spacer></v-spacer>
+        <v-btn icon color="black" @click="dialog = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-row no-gutters>
+        <v-img
+            :src="picture.Pic"
+            :height="picture.PicHeight"
+            :width="picture.PicWidth"
+            contain></v-img>
+      </v-row>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "DialogComponent",
+  data: () => ({
+    dialog: false,
+  }),
+};
+</script>
+
+<style>
+</style>
